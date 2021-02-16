5800a75ae9c5fd94d2d6a3b38feb45528cb4c2fa
Neem <spc1201@westminstercollege.edu>
2021-02-16 15:38:55 -0700

    Added test for score solution

--- TestWord.java
+++ TestWord.java
@@ -12,7 +12,7 @@ class TestWord {
 	 */
 	@Test
 	public void testConstructor() {
-		Word test = new Word("Hello");
+		Word test = new Word("hello");
 		Word test2 = new Word("test");
 		Word test3 = new Word("alphabetical");
 		Word test4 = new Word("goodbye");
@@ -23,12 +23,12 @@ class TestWord {
 	 */
 	@Test
 	public void testOriginal() {
-		Word test = new Word("Hello");
+		Word test = new Word("hello");
 		Word test2 = new Word("test");
 		Word test3 = new Word("alphabetical");
 		Word test4 = new Word("goodbye");
 		
-		assertEquals(true, test.getOriginalWord().equals("Hello"));
+		assertEquals(true, test.getOriginalWord().equals("hello"));
 		assertEquals(true, test2.getOriginalWord().equals("test"));
 		assertEquals(true, test3.getOriginalWord().equals("alphabetical"));
 		assertEquals(true, test4.getOriginalWord().equals("goodbye"));
@@ -39,15 +39,26 @@ class TestWord {
 	 */
 	@Test
 	public void testScramble() {
-		Word test = new Word("Hello");
+		Word test = new Word("hello");
 		Word test2 = new Word("test");
 		Word test3 = new Word("alphabetical");
 		Word test4 = new Word("goodbye");
 		
-		assertEquals(false, test.getScrambledWord().equals("Hello"));
+		assertEquals(false, test.getScrambledWord().equals("hello"));
 		assertEquals(false, test2.getScrambledWord().equals("test"));
 		assertEquals(false, test3.getScrambledWord().equals("alphabetical"));
 		assertEquals(false, test4.getScrambledWord().equals("goodbye"));	
+	}
+	
+	@Test
+	public void testScoreSolution()  {
+		Word test = new Word("hello");
+		
+		assertTrue(5, test.scoreSolution("hello"));
+		assertFalse(0, test.scoreSolution("elhlo"));
+		assertFalse(0, test.scoreSolution("heoll"));
+		assertFalse(0, test.scoreSolution("elohl"));
+		assertFalse(0, test.scoreSolution("helol"));
 	}
 
 }
