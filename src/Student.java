69741f0226b3a9b1d07d47570301d36bf07bd7d1
Neem <spc1201@westminstercollege.edu>
2021-02-18 13:20:52 -0700

    Added test files

---
+++ src/Student.java
@@ -0,0 +1,10 @@
+public class Student {
+
+	private String firstName;
+	private String lastName;
+	
+	public Student(String fname, String lname) {
+		this.firstName = fname;
+		this.lastName = lname;
+	}
+}
